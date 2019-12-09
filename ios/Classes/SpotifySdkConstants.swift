20b224ea965925c7de057a2e325a353cd4c1ae12
Steffen Gruschka <s.gruschka@salt-and-pepper.eu>
2019-12-09 08:50:28 +0100

    began iOS implementation

---
+++ ios/Classes/SpotifySdkConstants.swift
@@ -0,0 +1,44 @@
+//
+//  SpotifySdkConstants.swift
+//  Pods-Runner
+//
+//  Created by Steffen Gruschka on 29.11.19.
+//
+
+import Foundation
+
+public class SpotfySdkConstants
+{
+    //connecting
+    public static let methodConnectToSpotify = "connectToSpotify"
+    public static let methodGetAuthenticationToken = "getAuthenticationToken"
+    public static let methodLogoutFromSpotify = "logoutFromSpotify"
+
+    //player api
+    public static let methodQueueTrack = "queueTrack"
+    public static let methodPlay = "play"
+    public static let methodPause = "pause"
+    public static let methodResume = "resume"
+    public static let methodToggleRepeat = "toggleRepeat"
+    public static let methodToggleShuffle = "toggleShuffle"
+    public static let methodSkipNext = "skipNext"
+    public static let methodSkipPrevious = "skipPrevious"
+    public static let methodSeekTo = "seekTo"
+    public static let methodSeekToRelativePosition = "seekToRelativePosition"
+
+    //user api
+    public static let methodAddToLibrary = "addToLibrary"
+
+    //images api
+    public static let methodGetImage = "getImage"
+
+    public static let paramClientId = "clientId"
+    public static let paramRedirectUrl = "redirectUrl"
+    public static let paramSpotifyUri = "spotifyUri"
+    public static let paramImageUri = "imageUri"
+    public static let paramImageDimension = "imageDimension"
+    public static let paramPositionedMilliseconds = "positionedMilliseconds"
+    public static let paramRelativeMilliseconds = "relativeMilliseconds"
+}
+
+
--- ios/Classes/SwiftSpotifySdkPlugin.swift
+++ ios/Classes/SwiftSpotifySdkPlugin.swift
@@ -10,6 +10,29 @@ public class SwiftSpotifySdkPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    
+    guard let swiftArguments = call.arguments as? [String:Any] else {
+        result(FlutterError(code: "WROOOOOONG", message: "Could not convert arguments", details: nil))
+        return;
+    }
+
+    
+    switch call.method {
+    case SpotfySdkConstants.methodConnectToSpotify:
+        guard let clientID = swiftArguments[SpotfySdkConstants.paramClientId] as? String,
+        let url =  swiftArguments[SpotfySdkConstants.paramRedirectUrl] as? String else {
+            result(FlutterError(code: "WROOOOOONG", message: "Arguments of wrong type", details: nil))
+            return;
+        }
+        connectToSpotify(clientId: "",redirectUrl: "")
+        break;
+    default:
+        result(FlutterError(code: "404", message: "Not Method found", details: nil));
+        break;
+    }
   }
+    
+    private func connectToSpotify(clientId: String, redirectUrl: String) {
+        print("Good Job!")
+    }
 }
