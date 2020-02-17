fdb7546d025e9ee92b0ebb64624c0145e3623fce
Mateusz Rębacz <matirebacz@op.pl>
2020-02-17 16:19:35 +0100

    Created Dart wrapping around the entire Spotify JS API. Implemented basic connectToSpotify.

--- lib/spotify_sdk_web.dart
+++ lib/spotify_sdk_web.dart
@@ -5,23 +5,25 @@ import 'dart:developer';
 import 'dart:js';
 import 'dart:html';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'package:js/js.dart';
+import 'package:js/js_util.dart';
 
 class SpotifySdkPlugin {
-  // event streams
+  // event channels
   static const String CHANNEL_NAME = "spotify_sdk";
   static const String PLAYER_CONTEXT_SUBSCRIPTION = "player_context_subscription";
   static const String PLAYER_STATE_SUBSCRIPTION = "player_state_subscription";
   static const String CAPABILITIES__SUBSCRIPTION = "capabilities_subscription";
   static const String USER_STATUS_SUBSCRIPTION = "user_status_subscription";
 
-  //static const String playerContextChannel = EventChannel(registrar.messenger(), PLAYER_CONTEXT_SUBSCRIPTION)
-  //static const String playerStateChannel = EventChannel(registrar.messenger(), PLAYER_STATE_SUBSCRIPTION)
-  //static const String capabilitiesChannel = EventChannel(registrar.messenger(), CAPABILITIES__SUBSCRIPTION)
-  //static const String userStatusChannel = EventChannel(registrar.messenger(), USER_STATUS_SUBSCRIPTION)
+  static const EventChannel EVENT_CHANNEL_PLAYER_CONTEXT = EventChannel(PLAYER_CONTEXT_SUBSCRIPTION);
+  static const EventChannel EVENT_CHANNEL_PLAYER_STATE = EventChannel(PLAYER_STATE_SUBSCRIPTION);
+  static const EventChannel EVENT_CHANNEL_CAPABILITIES = EventChannel(CAPABILITIES__SUBSCRIPTION);
+  static const EventChannel EVENT_CHANNEL_USER_STATUS = EventChannel(USER_STATUS_SUBSCRIPTION);
 
   // connecting
   static const String METHOD_CONNECT_TO_SPOTIFY = "connectToSpotify";
@@ -84,6 +86,18 @@ class SpotifySdkPlugin {
   private var spotifyUserApi: SpotifyUserApi? = null
   private var spotifyImagesApi: SpotifyImagesApi? = null*/
 
+  /// Dio http client
+  final Dio _dio = Dio();
+
+  /// Whether the Spotify SDK was already loaded.
+  bool _sdkLoaded = false;
+  /// Current Spotify SDK player instance;
+  Player _currentPlayer;
+  /// Current WebPlaybackPlayer instance
+  WebPlaybackPlayer _currentPlayerConnection;
+
+  static const String ACCESS_TOKEN = 'BQCphbBlirQlququgGwllsUGqX4IJ5lOSAHw86M2xSRmuQAesZvu3TDr4l2y6fH2dYP7yYLLVbFvvrHoXNOgbYWWcPRZbTqZq_6ohbATCrmfytjzy5sYPTHRgyCi5IcesTxAVTb70MFTCUiUfbGPqVdNON0hQOTWO1L1SM2S6yVEMw';
+
   SpotifySdkPlugin() {
     _initializeSpotify();
   }
@@ -95,26 +109,319 @@ class SpotifySdkPlugin {
   }
 
   Future<dynamic> handleMethodCall(MethodCall call) async {
-    log('Method: ${call.method}');
+    // check if spotify is loaded
+    if(_sdkLoaded == false) {
+      throw PlatformException(
+        code: 'Uninitialized',
+        details: "The Spotify SDK wasn't initialized yet"
+      );
+    }
+
     switch (call.method) {
+      case METHOD_CONNECT_TO_SPOTIFY:
+        log('Connecting to Spotify...');
+        if(_currentPlayer != null) {
+          return true;
+        }
+
+        _currentPlayer = Player(
+          PlayerOptions(
+            name: "Test",
+            getOAuthToken: allowInterop((Function callback, t) {
+              callback(ACCESS_TOKEN);
+            })
+          )
+        );
+        
+        _registerPlayerEvents(_currentPlayer);
+        var result = await promiseToFuture(_currentPlayer.connect());
+        log('Connection result: $result');
+        return result;
+      break;
+      case METHOD_LOGOUT_FROM_SPOTIFY:
+        log('Disconnecting from Spotify...');
+        if(_currentPlayer == null) {
+          return false;
+        } else {
+          _unregisterPlayerEvents(_currentPlayer);
+          _currentPlayer.disconnect();
+          return true;
+        }
+      break;
+      case METHOD_RESUME:
+        if(_currentPlayer == null) {
+          return false;
+        } else {
+          await _playTrack('spotify:track:1DMEzmAoQIikcL52psptQL');
+          return true;
+        }
+      break;
       case METHOD_PAUSE:
-        log('Starting Spotify playback...');
+        if(_currentPlayer == null) {
+          return false;
+        } else {
+          await promiseToFuture(_currentPlayer.pause());
+          return true;
+        }
       break;
       default:
         throw PlatformException(
-            code: 'Unimplemented',
-            details: "The spotify_sdk plugin for web doesn't implement "
-                "the method '${call.method}'");
+          code: 'Unimplemented',
+          details: "The spotify_sdk plugin for web doesn't implement the method '${call.method}'");
     }
+  }
+
+  /// Starts track playback in Spotify
+  _playTrack(String uri) async {
+    if(_currentPlayerConnection == null) {
+      log('Spotify player not connected!');
+      return;
+    }
+
+    await _dio.put('https://api.spotify.com/v1/me/player/play',
+      data: { 'uris': [uri] },
+      queryParameters: {
+        'device_id': _currentPlayerConnection.device_id
+      },
+      options: Options(
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ${ACCESS_TOKEN}'
+        },
+      ),
+    );
   }
 
   /// Loads the Spotify SDK library.
   _initializeSpotify() {
-    context['onSpotifyWebPlaybackSDKReady'] = _onSpotifyInitialized();
+    context['onSpotifyWebPlaybackSDKReady'] = allowInterop(_onSpotifyInitialized);
     querySelector('body').children.add(ScriptElement()..src=SPOTIFY_SDK_URL);
   }
   /// Called when the Spotify library is loaded.
   _onSpotifyInitialized() {
     log('Spotify Initialized!');
+    _sdkLoaded = true;
   }
+  /// Registers Spotify event handlers.
+  _registerPlayerEvents(Player player) {
+    // player state
+    player.addListener(
+      'player_state_changed',
+      allowInterop(
+        (WebPlaybackState state) {
+          log('Player state: ${state.track_window.current_track.name}');
+        }
+      )
+    );
+
+    // ready/not ready
+    player.addListener(
+      'ready',
+      allowInterop(
+        (WebPlaybackPlayer player) {
+          log('Device ready! ${player?.device_id}');
+          _currentPlayerConnection = player;
+        }
+      )
+    );
+    player.addListener(
+      'not_ready',
+      allowInterop(
+        (event) {
+          log('Device not ready!');
+          _currentPlayerConnection = null;
+        }
+      )
+    );
+
+    // errors
+    player.addListener(
+      'initialization_error',
+      allowInterop(
+        (WebPlaybackError error) {
+          log('initialization_error: ${error.message}');
+        }
+      )
+    );
+    player.addListener(
+      'authentication_error',
+      allowInterop(
+        (WebPlaybackError error) {
+          log('authentication_error: ${error.message}');
+        }
+      )
+    );
+    player.addListener(
+      'account_error',
+      allowInterop(
+        (WebPlaybackError error) {
+          log('account_error: ${error.message}');
+        }
+      )
+    );
+    player.addListener(
+      'playback_error',
+      allowInterop(
+        (WebPlaybackError error) {
+          log('playback_error: ${error.message}');
+        }
+      )
+    );
+  }
+  _unregisterPlayerEvents(Player player) {
+    player.removeListener(
+      'player_state_changed'
+    );
+    player.removeListener(
+      'ready'
+    );
+    player.removeListener(
+      'not_ready'
+    );
+    player.removeListener(
+      'initialization_error'
+    );
+    player.removeListener(
+      'authentication_error'
+    );
+    player.removeListener(
+      'account_error'
+    );
+    player.removeListener(
+      'playback_error'
+    );
+  }
+}
+
+/// Spotify Player Object
+@JS('Spotify.Player')
+class Player {
+  /// The main constructor for initializing the Web Playback SDK. It should contain an object with the player name, volume and access token.
+  external Player(PlayerOptions options);
+  /// Connects Web Playback SDK instance to Spotify with the credentials provided during initialization.
+  external dynamic connect();
+  /// Closes the current session that Web Playback SDK has with Spotify.
+  external void disconnect(); 
+  /// Create a new event listener in the Web Playback SDK.
+  external void addListener(String type, Function callback);
+  /// Remove an event listener in the Web Playback SDK.
+  external void removeListener(String event_name);
+  /// Collect metadata on local playback.
+  external dynamic getCurrentState();
+  /// Rename the Spotify Player device. This is visible across all Spotify Connect devices.
+  external dynamic setName(String name);
+  /// Set the local volume for the Web Playback SDK.
+  external dynamic setVolume(double volume);
+  /// Pause the local playback.
+  external dynamic pause();
+  /// Resume the local playback.
+  external dynamic resume();
+  /// Resume/pause the local playback.
+  external dynamic togglePlay();
+  /// Seek to a position in the current track in local playback.
+  external dynamic seek(int position_ms);
+  /// Switch to the previous track in local playback.
+  external dynamic previousTrack();
+  /// Skip to the next track in local playback.
+  external dynamic nextTrack();
+}
+@JS()
+@anonymous
+class PlayerOptions {
+  external String get name;
+  external Function get getOAuthToken;
+  external double get volume;
+
+  external factory PlayerOptions({String name, Function getOAuthToken, double volume});
+}
+@JS('WebPlaybackPlayer')
+class WebPlaybackPlayer {
+  external String get device_id;
+
+  external factory WebPlaybackPlayer({String device_id});
+}
+@JS('WebPlaybackState')
+class WebPlaybackState  {
+  external WebPlayerContext get context;
+  external WebPlayerDisallows get disallows;
+  external bool get paused;
+  external int get position;
+  external int get repeat_mode;
+  external bool get shuffle;
+  external WebPlayerTrackWindow get track_window;
+
+  external factory WebPlaybackState({WebPlayerContext context, WebPlayerDisallows disallows, bool paysed, int position, int repeat_mode, bool shuffle, WebPlayerTrackWindow track_window});
+}
+@JS()
+@anonymous
+class WebPlayerContext  {
+  external String get uri;
+  external Map get metadata;
+
+  external factory WebPlayerContext({String uri, Map metadata});
+}
+@JS()
+@anonymous
+class WebPlayerDisallows  {
+  external bool get pausing;
+  external bool get peeking_next;
+  external bool get peeking_prev;
+  external bool get resuming;
+  external bool get seeking;
+  external bool get skipping_next;
+  external bool get skipping_prev;
+
+  external factory WebPlayerDisallows({bool pausing, bool peeking_next, bool peeking_prev, bool resuming, bool seeking, bool skipping_next, bool skipping_prev});
+}
+@JS()
+@anonymous
+class WebPlayerTrackWindow  {
+  external WebPlaybackTrack get current_track;
+  external List<WebPlaybackTrack> get previous_tracks;
+  external List<WebPlaybackTrack> get next_tracks;
+
+  external factory WebPlayerTrackWindow({WebPlaybackTrack current_track, List<WebPlaybackTrack> previous_tracks, List<WebPlaybackTrack> next_tracks});
+}
+@JS('WebPlaybackTrack')
+class WebPlaybackTrack {
+  external String get uri;
+  external String get id;
+  external String get type;
+  external String get media_type;
+  external String get name;
+  external bool get is_playable;
+  external WebPlaybackAlbum get album;
+  external List<WebPlaybackArtist> get artists;
+
+  external factory WebPlaybackTrack({String uri, String id, String type, String media_type, String name, bool is_playable, WebPlaybackAlbum album, List<WebPlaybackArtist> artists});
+}
+@JS()
+@anonymous
+class WebPlaybackAlbum {
+  external String get uri;
+  external String get name;
+  external List<WebPlaybackAlbumImage> get images;
+
+  external factory WebPlaybackAlbum({String uri, String name, List<WebPlaybackAlbumImage> images});
+}
+@JS()
+@anonymous
+class WebPlaybackArtist {
+  external String get uri;
+  external String get name;
+
+  external factory WebPlaybackArtist({String uri, String name});
+}
+@JS()
+@anonymous
+class WebPlaybackAlbumImage {
+  external String get url;
+
+  external factory WebPlaybackAlbumImage({String url});
+}
+@JS('WebPlaybackError')
+class WebPlaybackError {
+  external String get message;
+
+  external factory WebPlaybackError({String message});
 }
