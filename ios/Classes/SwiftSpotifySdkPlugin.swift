98142df6028a991ff74f3a002e769a9e4c74240f
Foti Dim <fdimanidis@gmail.com>
2020-07-12 23:06:59 +0200

    Implement Dart-Swift communication

--- ios/Classes/SwiftSpotifySdkPlugin.swift
+++ ios/Classes/SwiftSpotifySdkPlugin.swift
@@ -3,36 +3,36 @@ import UIKit
 
 public class SwiftSpotifySdkPlugin: NSObject, FlutterPlugin {
     
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "spotify_sdk", binaryMessenger: registrar.messenger())
-    let instance = SwiftSpotifySdkPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let spotifySDKChannel = FlutterMethodChannel(name: "spotify_sdk", binaryMessenger: registrar.messenger())
+        let connectionStatusChannel = FlutterEventChannel(name: "connection_status_subscription", binaryMessenger: registrar.messenger())
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    
-    guard let swiftArguments = call.arguments as? [String:Any] else {
-        result(FlutterError(code: "WROOOOOONG", message: "Could not convert arguments", details: nil))
-        return;
+        let instance = SwiftSpotifySdkPlugin()
+
+        registrar.addMethodCallDelegate(instance, channel: spotifySDKChannel)
+        connectionStatusChannel.setStreamHandler(ConnectionStatusHandler())
     }
 
-    
-    switch call.method {
-    case SpotfySdkConstants.methodConnectToSpotify:
-        guard let clientID = swiftArguments[SpotfySdkConstants.paramClientId] as? String,
-        let url =  swiftArguments[SpotfySdkConstants.paramRedirectUrl] as? String else {
-            result(FlutterError(code: "WROOOOOONG", message: "Arguments of wrong type", details: nil))
-            return;
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let swiftArguments = call.arguments as? [String:Any] else {
+            result(FlutterError(code: "Arguments Error", message: "Could not parse arguments", details: nil))
+            return
         }
-        connectToSpotify(clientId: "",redirectUrl: "")
-        break;
-    default:
-        result(FlutterError(code: "404", message: "Not Method found", details: nil));
-        break;
+
+        switch call.method {
+        case SpotfySdkConstants.methodConnectToSpotify:
+            guard let clientID = swiftArguments[SpotfySdkConstants.paramClientId] as? String,
+                let url =  swiftArguments[SpotfySdkConstants.paramRedirectUrl] as? String else {
+                    result(FlutterError(code: "Arguments Error", message: "One or more arguments are missing", details: nil))
+                    return
+            }
+            connectToSpotify(clientId: clientID, redirectURL: url)
+        default:
+            result(FlutterError(code: "404", message: "No Method found", details: nil))
+        }
     }
-  }
     
-    private func connectToSpotify(clientId: String, redirectUrl: String) {
-        print("Good Job!")
+    private func connectToSpotify(clientId: String, redirectURL: String) {
+        print("ClientID: \(clientId), RedirectURL: \(redirectURL)")
     }
 }
