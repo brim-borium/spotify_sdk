21b96e1682ce944001391ae2aced0787b9ea908f
Mateusz Rębacz <matirebacz@op.pl>
2020-09-12 20:01:45 +0200

    Update ConnectionStatusHandler.swift

--- ios/Classes/ConnectionStatusHandler.swift
+++ ios/Classes/ConnectionStatusHandler.swift
@@ -9,7 +9,7 @@ class ConnectionStatusHandler: StatusHandler, SPTAppRemoteDelegate {
     func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
         if error {
             // report spotify remote error to plugin
-            eventSink?("{\"connected\": false, \"errorCode\": \(error.code), \"errorDetails\": \(error.localizedDescription)}")
+            eventSink?("{\"connected\": false, \"errorCode\": \(error!.code), \"errorDetails\": \(error!.localizedDescription)}")
         } else {
             // report disconnection to plugin
             eventSink?("{\"connected\": false}")
@@ -19,7 +19,7 @@ class ConnectionStatusHandler: StatusHandler, SPTAppRemoteDelegate {
     func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
         if error {
             // report spotify remote error to plugin
-            eventSink?("{\"connected\": false, \"errorCode\": \(error.code), \"errorDetails\": \(error.localizedDescription)}")
+            eventSink?("{\"connected\": false, \"errorCode\": \(error!.code), \"errorDetails\": \(error!.localizedDescription)}")
         } else {
             // report disconnection to plugin
             eventSink?("{\"connected\": false}")
