3c6b8f5c74a59b6afcececa5418f9e1d1b4f6176
Foti Dim <fdimanidis@gmail.com>
2020-09-19 13:14:22 +0200

    Reset FlutterResult to nil

--- ios/Classes/ConnectionStatusHandler.swift
+++ ios/Classes/ConnectionStatusHandler.swift
@@ -9,9 +9,17 @@ class ConnectionStatusHandler: StatusHandler, SPTAppRemoteDelegate {
         connectionResult?(true)
         tokenResult?(appRemote.connectionParameters.accessToken)
         eventSink?("{\"connected\": true}")
+
+        connectionResult = nil
+        tokenResult = nil
     }
 
     func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+        defer {
+            connectionResult = nil
+            tokenResult = nil
+        }
+
         if error != nil {
             // report spotify remote error to plugin
             eventSink?("{\"connected\": false, \"errorCode\": \"\(error!._code)\", \"errorDetails\": \"\(error!.localizedDescription)\"}")
@@ -29,13 +37,9 @@ class ConnectionStatusHandler: StatusHandler, SPTAppRemoteDelegate {
         if error != nil {
             // report spotify remote error to plugin
             eventSink?("{\"connected\": false, \"errorCode\": \"\(error!._code)\", \"errorDetails\": \"\(error!.localizedDescription)\"}")
-            connectionResult?(FlutterError(code: String(error!._code), message: error!.localizedDescription, details: nil))
-            tokenResult?(FlutterError(code: String(error!._code), message: error!.localizedDescription, details: nil))
         } else {
             // report disconnection to plugin
             eventSink?("{\"connected\": false}")
-            connectionResult?(FlutterError(code: String(error!._code), message: error!.localizedDescription, details: nil))
-            tokenResult?(FlutterError(code: "errorConnection", message: "Failed Connection Attempt", details: nil))
         }
     }
 }
