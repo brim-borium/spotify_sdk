408ae64ec71538927b1701b9ef157a562f0cb2ca
Foti Dim <fdimanidis@gmail.com>
2020-09-20 19:06:53 +0200

    Streamline iOS connection & token flow (#49)
    * Streamline iOS connection & token flow
    
    * Reset FlutterResult to nil

--- ios/Classes/ConnectionStatusHandler.swift
+++ ios/Classes/ConnectionStatusHandler.swift
@@ -2,27 +2,34 @@ import SpotifyiOS
 
 class ConnectionStatusHandler: StatusHandler, SPTAppRemoteDelegate {
 
-    let pluginInstance : SwiftSpotifySdkPlugin
-    init(pluginInstance: SwiftSpotifySdkPlugin){
-        self.pluginInstance = pluginInstance
-    }
+    var tokenResult: FlutterResult?
+    var connectionResult: FlutterResult?
     
     func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
-        defer {
-            pluginInstance.connectionResult = nil
-        }
-        
-        pluginInstance.connectionResult?(true)
+        connectionResult?(true)
+        tokenResult?(appRemote.connectionParameters.accessToken)
         eventSink?("{\"connected\": true}")
+
+        connectionResult = nil
+        tokenResult = nil
     }
 
     func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+        defer {
+            connectionResult = nil
+            tokenResult = nil
+        }
+
         if error != nil {
             // report spotify remote error to plugin
             eventSink?("{\"connected\": false, \"errorCode\": \"\(error!._code)\", \"errorDetails\": \"\(error!.localizedDescription)\"}")
+            connectionResult?(FlutterError(code: String(error!._code), message: error!.localizedDescription, details: nil))
+            tokenResult?(FlutterError(code: String(error!._code), message: error!.localizedDescription, details: nil))
         } else {
             // report disconnection to plugin
             eventSink?("{\"connected\": false}")
+            connectionResult?(FlutterError(code: "errorConnection", message: "Failed Connection Attempt", details: nil))
+            tokenResult?(FlutterError(code: "errorConnection", message: "Failed Connection Attempt", details: nil))
         }
     }
 
