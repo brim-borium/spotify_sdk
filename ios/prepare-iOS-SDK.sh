b509f7d34028663c9483f38e4a5380041e527178
Foti Dim <fdimanidis@gmail.com>
2021-11-27 17:30:23 +0100

    Switch to native Spotify iOS XCFramework (#140)

--- ios/prepare-iOS-SDK.sh
+++ ios/prepare-iOS-SDK.sh
@@ -1,27 +1,10 @@
 #!/bin/sh
+REPO_NAME="ios-sdk"
+FRAMEWORK_NAME="SpotifyiOS.xcframework"
 
-if [ "$#" -eq 1 ]; then
-    BASE_DIR=$1
-else
-BASE_DIR=$(exec pwd)
-fi
-
-rm -fR ios-sdk
-mkdir ios-sdk
-curl -OL https://github.com/spotify/ios-sdk/archive/v1.2.2.zip
-unzip -o v1.2.2.zip
-mv ios-sdk-1.2.2/SpotifyiOS.framework ios-sdk
-rm v1.2.2.zip
-rm -fR ios-sdk-1.2.2
-
-BASE_DIR="${BASE_DIR}/ios-sdk"
-echo "BASE_DIR: ${BASE_DIR}"
-
-MODULE_DIR="${BASE_DIR}/SpotifyiOS.framework"
-echo "MODULE_DIR: ${MODULE_DIR}"
-mkdir -p "${MODULE_DIR}"
-printf "module SpotifyiOS {\n\
-header \"Headers/SpotifyiOS.h\"\n\
-export *\n\
-}" > "${MODULE_DIR}/module.map"
-echo "Created module map."
+#!/bin/sh
+rm -fR ${REPO_NAME}
+mkdir ${REPO_NAME}
+git clone https://github.com/spotify/${REPO_NAME}
+git -C ${REPO_NAME} checkout f9a7d53
+find ./${REPO_NAME} -mindepth 1 -maxdepth 1 -not -name ${FRAMEWORK_NAME} -exec rm -rf '{}' \;   # Keep on only the xcframework folder
--- ios/spotify_sdk.podspec
+++ ios/spotify_sdk.podspec
@@ -20,7 +20,7 @@ Unofficial Spotify Flutter SDK.
   # Flutter.framework does not contain a i386 slice. Only x86_64 simulators are supported.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'VALID_ARCHS[sdk=iphonesimulator*]' => 'x86_64' }
   s.swift_version = '5.0'
-  s.preserve_paths = 'ios-sdk/SpotifyiOS.framework'
-  s.vendored_frameworks = 'ios-sdk/SpotifyiOS.framework'
   s.prepare_command = './prepare-iOS-SDK.sh'
+  s.vendored_frameworks = 'ios-sdk/SpotifyiOS.xcframework'
+  s.preserve_paths = 'ios-sdk/SpotifyiOS.xcframework'
   end
