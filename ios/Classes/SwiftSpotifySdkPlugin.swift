20d6f851735e82be434652187ed077efad01c885
Hashem <hashemp206@yahoo.com>
2024-10-03 22:41:45 +0100

    Call `AuthorizeAndPlayURI` on main thread (#221)
    * fix: call a`AuthorizeAndPlayURI` on main thread which internally calls `UIAppliation.openURL`
    
    * set min supported iOS version to 12.0
    
    * fix the indentation

--- ios/Classes/SwiftSpotifySdkPlugin.swift
+++ ios/Classes/SwiftSpotifySdkPlugin.swift
@@ -366,13 +366,13 @@ public class SwiftSpotifySdkPlugin: NSObject, FlutterPlugin {
         if accessToken != nil {
             appRemote?.connect()
         } else {
-          let appRemote = self.appRemote
-          Task {
-            // Note: A blank string will play the user's last song or pick a random one.
-            if await appRemote?.authorizeAndPlayURI(spotifyUri, asRadio: asRadio ?? false, additionalScopes: scopes) == false {
-              throw SpotifyError.spotifyNotInstalledError
+          // Note: A blank string will play the user's last song or pick a random one.
+          self.appRemote?.authorizeAndPlayURI(spotifyUri, asRadio: asRadio ?? false, additionalScopes: scopes) { success in
+            if (!success) {
+              self.connectionStatusHandler?.connectionResult?(FlutterError(code: "spotifyNotInstalled", message: "Spotify app is not installed", details: nil))
             }
           }
+        }
     }
 }
 
