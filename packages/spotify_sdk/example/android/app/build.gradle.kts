26ca9a7cea651a9290b2e98f919e4372c24a2d24
Tobias Busch <tobi@minimalme.de>
2026-07-19 13:42:19 +0200

    Update Android SDK and track iOS Podfiles
    * Bump Android `compileSdk` and `targetSdk` to 36
    * Include `Podfile` and `Podfile.lock` in the example app
    * Update `verify_pub_score.sh` threshold to 140 in CI workflows
    * Update `.gitignore` to stop ignoring Podfiles and generated files

--- packages/spotify_sdk/example/android/app/build.gradle.kts
+++ packages/spotify_sdk/example/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "de.minimalme.spotify_sdk_example"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -21,7 +21,7 @@ android {
     defaultConfig {
         applicationId = "de.minimalme.spotify_sdk_example"
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
