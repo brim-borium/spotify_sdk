616bc58c947cefca1ba04eb15f46e37642dbe871
Tobias Busch <tobi@minimalme.de>
2026-07-19 21:14:47 +0200

    fix(ios): import correct Swift header for renamed pod name

--- packages/spotify_sdk_ios/ios/Classes/SpotifySdkPlugin.m
+++ packages/spotify_sdk_ios/ios/Classes/SpotifySdkPlugin.m
@@ -1,11 +1,11 @@
 #import "SpotifySdkPlugin.h"
-#if __has_include(<spotify_sdk/spotify_sdk-Swift.h>)
-#import <spotify_sdk/spotify_sdk-Swift.h>
+#if __has_include(<spotify_sdk_ios/spotify_sdk_ios-Swift.h>)
+#import <spotify_sdk_ios/spotify_sdk_ios-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "spotify_sdk-Swift.h"
+#import "spotify_sdk_ios-Swift.h"
 #endif
 
 #import <SpotifyiOS/SpotifyiOS.h>
