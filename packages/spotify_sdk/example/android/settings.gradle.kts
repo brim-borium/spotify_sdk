156170389b34da6730f0eadc616750f2390defb6
Tobias Busch <tobi@minimalme.de>
2026-08-08 10:59:23 +0200

    fix(android): resolve Kotlin version warning and suppress AGP 9 DSL warnings

--- packages/spotify_sdk/example/android/settings.gradle.kts
+++ packages/spotify_sdk/example/android/settings.gradle.kts
@@ -20,6 +20,7 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.3.1" apply false
     id("com.android.library") version "9.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 include(":app")
