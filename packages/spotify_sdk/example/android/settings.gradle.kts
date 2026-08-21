d2e315a2e383f1252c1d05c8277d4aae9a511a61
Tobias Busch <tobi@minimalme.de>
2026-08-21 11:16:44 +0200

    chore(deps): incorporate GitHub Actions and Android Gradle/Kotlin dependency bumps
    - Merge GitHub actions updates from #266 (sticky-pull-request-comment@v3, actions/upload-artifact@v7, codecov/codecov-action@v7).
    - Merge Android updates from #267 (gradle wrapper 9.7.0, jackson-annotations 2.22, kotlin 2.4.10).

--- packages/spotify_sdk/example/android/settings.gradle.kts
+++ packages/spotify_sdk/example/android/settings.gradle.kts
@@ -20,7 +20,7 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.3.1" apply false
     id("com.android.library") version "9.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
 }
 
 include(":app")
